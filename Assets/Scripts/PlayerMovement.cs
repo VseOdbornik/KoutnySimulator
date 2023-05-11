e645d6a7716fcce26397c101a2a0904517a1b759
VseOdbornik <DAVIDborsiceCZ@seznam.cz>
2023-05-11 19:20:50 +0200

    Made the code not completly stupid

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,21 +9,27 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Transform groundCheck;
     [SerializeField] Transform wallCheck;
 
-    const int walkCoeficient = 300;
-    const int jumpCoeficient = 11;
+    [SerializeField] float walkCoefficient = 300;
+    [SerializeField] float jumpCoefficient = 11;
+    [SerializeField] float wallJumpCoefficient = 1.8f;
 
-    float movementX;
-    float movementY;
-    float lastMovementX = 1;
+    float facingDirectionX = 1;
     Vector2 dir;
 
     [SerializeField] float movementSpeed = 1;
     [SerializeField] float jumpForce = 1;
+    [SerializeField] float slideSpeed;
+    [SerializeField] float wallJumpTime = .5f;
+    [SerializeField] int wallJumpMoveSpeed;
 
     [SerializeField] float coyoteTime = .1f;
     [SerializeField] float coyoteTimeCounter = .1f;
 
-    bool isHolding;
+    [SerializeField] bool onGround;
+    [SerializeField] bool onWall;
+    [SerializeField] bool isHolding;
+    [SerializeField] bool cantWalk;
+    [SerializeField] bool wallJumped;
 
     private void Start()
     {
@@ -32,50 +38,124 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        movementX = Input.GetAxisRaw("Horizontal");
-        movementY = Input.GetAxisRaw("Vertical");
-        dir = new Vector2(movementX, movementY);
-        if (movementX != 0) lastMovementX= movementX;
+        SetDirection();
 
-        Walk();
-        FlipCharacter(lastMovementX);
+        if (!cantWalk)
+        {
+            Walk();
+        }
     }
 
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.LeftShift) || !OnWall()) StopHolding();
-        if (Input.GetKey(KeyCode.LeftShift) && OnWall()) Hold();
-        if (Input.GetKeyDown(KeyCode.Space)) Jump();
+        SetBools();
 
-        if (OnGround()) coyoteTimeCounter = coyoteTime;
-        else coyoteTimeCounter -= Time.deltaTime;
+        if (Input.GetKeyUp(KeyCode.LeftShift) || !onWall)
+        {
+            StopHold();
+        }
 
+        if(Input.GetKey(KeyCode.LeftShift) && onWall && !wallJumped)
+        {
+
+            if (!isHolding) StartHolding();
+            //else Hold();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (onGround || coyoteTimeCounter > 0)
+            {
+                Jump();
+            }
+            else if(onWall)
+            {
+                WallJump();
+            } 
+        }
+        if (onGround)
+        {
+            coyoteTimeCounter = coyoteTime;
+            StopWallJump();
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
         if (Input.GetKeyUp(KeyCode.Space) && rb.velocity.y > 0) rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+    }
+
+    void SetDirection() 
+    {
+        dir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (dir.x != 0) facingDirectionX = dir.x;
+
+        if(!isHolding)
+        FlipCharacter(facingDirectionX);
+    }
+
+    void SetBools()
+    {
+        onGround = OnGround();
+        onWall = OnWall();
+        isHolding = IsHolding();
     }
 
     void Walk()
     {
-        rb.velocity = new Vector2(movementX * movementSpeed * Time.deltaTime * walkCoeficient, rb.velocity.y);
+        Vector2 walk = dir * movementSpeed * Time.deltaTime * walkCoefficient;
+
+        if (!isHolding)
+        {
+            rb.velocity = new Vector2(walk.x, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(rb.velocity.x, walk.y) / 2;
+        }
     }
 
     void Jump()
     {
-        if (!OnGround() && coyoteTimeCounter <= 0) return;
-
         coyoteTimeCounter = 0;
         rb.velocity = new Vector2(rb.velocity.x, 0);
-        rb.velocity += Vector2.up * jumpForce * jumpCoeficient;
+        rb.velocity += Vector2.up * jumpForce * jumpCoefficient;
+    }
+
+    void WallJump()
+    {
+        bool hold = isHolding;
+        StopHold();
+        StartWallJump(0.3f);
+
+        rb.velocity = Vector2.zero;
+        if (hold)
+        {
+            rb.velocity += new Vector2(dir.x, 1).normalized * jumpForce * jumpCoefficient;
+        }
+        else
+        {
+            rb.velocity += new Vector2(-facingDirectionX, 1).normalized * jumpForce * jumpCoefficient;
+        }
+    }
+
+    void StartHolding()
+    {
+        isHolding = true;
+        rb.velocity = Vector2.zero;
+        rb.gravityScale = 0;
     }
 
     void Hold()
     {
-        rb.gravityScale = 0;
-        rb.velocity = Vector2.zero;
+        //rb.velocity = new Vector2(0, rb.velocity.y);
     }
 
-    void StopHolding()
+    void StopHold()
     {
         rb.gravityScale = 2.2f;
+        isHolding = false;
     }
 
     void FlipCharacter(float leftOrRight)
@@ -87,7 +167,7 @@ public class PlayerMovement : MonoBehaviour
 
     bool OnGround()
     {
-        Collider2D[] colls = Physics2D.OverlapBoxAll(groundCheck.position, new Vector2(0.65f, 0.12f),0);
+        Collider2D[] colls = Physics2D.OverlapBoxAll(groundCheck.position, new Vector2(0.65f, 0.12f), 0);
         if (colls.Length > 1) return true;
         else return false;
     }
@@ -97,6 +177,42 @@ public class PlayerMovement : MonoBehaviour
         Collider2D[] colls = Physics2D.OverlapBoxAll(wallCheck.position, new Vector2(0.1f, 1f), 0);
         if (colls.Length > 1) return true;
         else return false;
+    }
+
+    bool IsHolding()
+    {
+        return onWall && Input.GetKeyDown(KeyCode.LeftShift);
+    }
+
+    void StartWallJump(float time)
+    {
+        StopCoroutine(WallJump(time));
+        StartCoroutine(WallJump(time));
+    }
+    IEnumerator WallJump(float time)
+    {
+        wallJumped = true;
+        yield return new WaitForSeconds(time);
+        wallJumped = false;
+    }
+
+    void StopWallJump()
+    {
+        StopCoroutine("WallJump");
+        wallJumped = false;
+    }
+
+    void StartCantWalk(float time)
+    {
+        StopCoroutine(CantWalk(time));
+        StartCoroutine(CantWalk(time));
+    }
+
+    IEnumerator CantWalk(float time)
+    {
+        cantWalk = true;
+        yield return new WaitForSeconds(time);
+        cantWalk = false;
     }
 
     private void OnDrawGizmos()
